c1e1cafa5b0a3d57e0b0f1b85856bc61d3cd994e
juanamezquita <t.amezquita.juancarlos@gmail.com>
2021-05-03 09:51:22 -0500

    segunda actualizacion

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div >
-      <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        
+class App extends Component {
+  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+ 
+ 
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+    );
+  }
+
 }
+
 
 export default App;
